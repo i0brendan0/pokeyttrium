24e75f3e8ec9ffda4049fb7158898318ea6aefe3
i0brendan0 <19826742+i0brendan0@users.noreply.github.com>
2025-05-05 10:13:07 -0500

    Noland Back Sprite

--- src/battle_controller_player.c
+++ src/battle_controller_player.c
@@ -1883,7 +1883,7 @@ u32 LinkPlayerGetTrainerPicId(u32 multiplayerId)
     else if (version == VERSION_RUBY || version == VERSION_SAPPHIRE)
         trainerPicId = gender + TRAINER_BACK_PIC_RUBY_SAPPHIRE_BRENDAN;
     else
-        trainerPicId = gender + TRAINER_BACK_PIC_BRENDAN;
+        trainerPicId = gender + TRAINER_BACK_PIC_NOLAND;
 
     return trainerPicId;
 }
@@ -1895,7 +1895,7 @@ static u32 PlayerGetTrainerBackPicId(void)
     if (gBattleTypeFlags & BATTLE_TYPE_LINK)
         trainerPicId = LinkPlayerGetTrainerPicId(GetMultiplayerId());
     else
-        trainerPicId = gSaveBlock2Ptr->playerGender + TRAINER_BACK_PIC_BRENDAN;
+        trainerPicId = gSaveBlock2Ptr->playerGender + TRAINER_BACK_PIC_NOLAND;
 
     return trainerPicId;
 }
